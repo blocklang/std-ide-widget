eba2898e5c23fc357a7e72e96e4fd8c8638f9ec5
xiaohulu <idocument@qq.com>
2019-09-20 09:16:54 +0800

    跑通 dojo build widget 命令

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,6 +1,6 @@
 import { create, tsx } from '@dojo/framework/core/vdom';
 import theme from '@dojo/framework/core/middleware/theme';
-import PageBase from 'std-widget-web/Page';
+//import PageBase from 'std-widget-web/Page';
 import * as css from './index.m.css';
 
 const factory = create({ theme });
@@ -8,6 +8,7 @@ const factory = create({ theme });
 export default factory(function Page({ middleware: { theme } }) {
     const {root} = theme.classes(css);
     return (
-        <PageBase classes={[root]}/>
+        //<PageBase classes={[root]}/>
+        <div classes={[root]}></div>
     );
 });

b286768f349dad05f4b1b96b3b83ddb908e2020e
xiaohulu <idocument@qq.com>
2019-09-21 20:38:24 +0800

    为便于本地开发，改为引用本地文件夹中的项目

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,14 +1,6 @@
-import { create, tsx } from '@dojo/framework/core/vdom';
-import theme from '@dojo/framework/core/middleware/theme';
-//import PageBase from 'std-widget-web/Page';
-import * as css from './index.m.css';
+import PageBase from 'std-widget-web/src/page';
+import WidgetDesignableMixin from 'designer-core/src/mixins/WidgetDesignable';
 
-const factory = create({ theme });
+export default class Page extends WidgetDesignableMixin(PageBase) {
 
-export default factory(function Page({ middleware: { theme } }) {
-    const {root} = theme.classes(css);
-    return (
-        //<PageBase classes={[root]}/>
-        <div classes={[root]}></div>
-    );
-});
+}

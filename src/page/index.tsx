1ea9068cd83bf7a99accc4fcf22101248b83fd3f
xiaohulu <idocument@qq.com>
2019-10-03 15:43:36 +0800

    为 Page 部件添加最小高度

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,7 +1,11 @@
-import PageBase from 'std-widget-web/page';
+import PageBase from "std-widget-web/page";
 // 注意：此处引入 default，在 build 时会有警告信息
-import {WidgetDesignableMixin} from 'designer-core/mixins/WidgetDesignable';
+import { WidgetDesignableMixin } from "designer-core/mixins/WidgetDesignable";
+
+import * as css from "./index.m.css";
 
 export default class Page extends WidgetDesignableMixin(PageBase) {
-
+	protected getRootCssClass() {
+		return css.root;
+	}
 }

abd04251dc613e43bced607d297f1ef76dc4cbdb
xiaohulu <idocument@qq.com>
2020-01-15 21:15:48 +0800

    将 Page 部件从 tsx 转换为 HyperScript

--- src/page/index.ts
+++ src/page/index.ts
@@ -1,4 +1,4 @@
-import { create, tsx } from "@dojo/framework/core/vdom";
+import { create, v } from "@dojo/framework/core/vdom";
 import { PageProperties } from "std-widget-web/page";
 import ide from "designer-core/middleware/ide";
 import * as css from "./index.m.css";
@@ -7,12 +7,10 @@ const factory = create({ ide }).properties<PageProperties>();
 
 export default factory(function Page({ children, middleware: { ide } }) {
 	ide.config("root");
-	ide.tryFocus();
 	const activeWidgetEvents = ide.activeWidgetEvents();
 
-	return (
-		<div key="root" classes={[css.root]} {...activeWidgetEvents}>
-			{children()}
-		</div>
-	);
+	return [
+		v("div", { key: "root", classes: [css.root], ...activeWidgetEvents }, children()),
+		ide.alwaysRenderActiveWidget()
+	];
 });
